d936e22d1a1ce1c23b0ca04225e0b56a169dbbf1
ViniciusMeirelles <vinicius.meirelles@outlook.com.br>
2018-06-17 14:41:26 -0300

    First commit. Buttons light up on click.

---
+++ BotaoCor.gd
@@ -0,0 +1,5 @@
+extends "res://Botoes/Botao.gd"
+
+func pressionaBotao():
+	if mouse_no_botao:
+		ligaBotao()
---
+++ Botoes/Botao.gd
@@ -0,0 +1,31 @@
+extends Node
+
+onready var luz = get_node("Light2D")
+onready var timer_luz = get_node("Timer_luz")
+var mouse_no_botao = false
+
+
+func _ready():
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		pressionaBotao()
+
+func pressionaBotao():
+	if mouse_no_botao:
+		ligaBotao()
+
+
+func ligaBotao():
+	luz.show()
+	timer_luz.start()
+
+func desligaBotao():
+	luz.hide()
+
+func mouse_entrou_botao():
+	mouse_no_botao = true
+
+func mouse_saiu_botao():
+	mouse_no_botao = false
